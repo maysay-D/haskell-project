c2fae19d14ffaa516eeb48d92aea20a9599523d6
maysay-D <22j5085@stu.meisei-u.ac.jp>
2024-09-16 01:43:35 +0900

    feat: 標準入力で受け取った数値を基に総和が表示されるプログラムを作成

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import System.IO (isEOF)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "Hello, Haskell!"
+    putStrLn "Any Numbers?"
+    sumNumbers 0
+
+sumNumbers :: Int -> IO ()
+sumNumbers acc = do
+    eof <- isEOF
+    if eof
+        then putStrLn $ "Sum: " ++ show acc
+        else do
+            input <- getLine
+            let number = read input :: Int
+            sumNumbers (acc + number)
